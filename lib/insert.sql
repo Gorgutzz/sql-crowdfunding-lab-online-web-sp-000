473ca878524f9fe04c9744868e881edd6f6653f6
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-17 23:32:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,17 @@
 INSERT INTO users(id, name, age)
-VALUES (1, 'a', 17), (2, 'b', 6), (3, 'c', 4), (4, 'd', 33),
-(5, 'e', 24), (6, 'f', 24), (7, 'g', 9), (8, 'h', 23), (9, 'i', 100), (10, 'j', 30),
-(11, 'k', 90), (12, 'l', 24), (13, 'm', 4), (14, 'n', 36), (15, 'o', 113), (16, 'p', 5),
-(17, 'q', 6), (18, 'r', 5), (19, 's', 24), (20, 't', 24);
+VALUES (1, 'Ash', 17), (2, 'Bill', 6), (3, 'Carl', 4), (4, 'Dan', 33),
+(5, 'Emelia', 24), (6, 'Frank', 24), (7, 'Gordon', 9), (8, 'Hank', 23), (9, 'Ian', 100), (10, 'Jon', 30),
+(11, 'Kate', 90), (12, 'Liam', 24), (13, 'Monty', 4), (14, 'Nina', 36), (15, 'Olly', 113), (16, 'Paul', 5),
+(17, 'Quan', 6), (18, 'Roger', 5), (19, 'Sam', 24), (20, 'Tina', 24);
 
 INSERT INTO projects(id, title, category, funding_goal, start_date, end_date)
-VALUES ()
+VALUES (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
