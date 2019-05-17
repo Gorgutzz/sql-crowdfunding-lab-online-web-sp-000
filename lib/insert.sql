17cb77aa0141ae10034e934fdc8baf370f48f996
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-17 23:38:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,13 +5,13 @@ VALUES (1, 'Ash', 17), (2, 'Bill', 6), (3, 'Carl', 4), (4, 'Dan', 33),
 (17, 'Quan', 6), (18, 'Roger', 5), (19, 'Sam', 24), (20, 'Tina', 24);
 
 INSERT INTO projects(id, title, category, funding_goal, start_date, end_date)
-VALUES (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
-(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
-(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
-(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
-(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
-(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
-(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
-(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
-(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
-(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+VALUES (1, 'Half Life 3', 'games', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'Windss of Winter', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'Audi', 'cars', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Greek Salad', 'food', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Pizza', 'food', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'Techno Viking', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'New Carpet', 'renovate', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'New weightlifting belt', 'gym', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'New chair', 'renovate', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Cyberpunk 2077', 'games', 5000.00, '2012-03-20', '2013-06-30');
